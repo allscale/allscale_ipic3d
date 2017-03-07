4484c073b5725239798e84bd0a99535693211d5f
Roman Iakymchuk <riakymch@kth.se>
2017-03-07 11:40:57 +0100

    Implemented curl of E as a part of the magnetic field solver (Faraday's law)

--- code/app/include/ipic3d/app/field.h
+++ code/app/include/ipic3d/app/field.h
@@ -19,6 +19,50 @@ namespace ipic3d {
 	using Field = allscale::api::user::data::Grid<FieldNode,3>;	// a 3D grid of field nodes
 
 	/**
+ 	* calculate curl on nodes, given a vector field defined on central points
+ 	*/
+	void curlC2N(){
+
+	}
+
+	/**
+ 	* calculate curl on central points, given a vector field defined on nodes
+ 	*/
+	void curlN2C(const UniverseProperties& universeProperties, const utils::Coordinate<3>& pos, const Field& field, Vector3<double> &curl) {
+		// extract electric field values from nodes of the cell
+		Vector3<double> Es[2][2][2];
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<2; j++) {
+				for(int k=0; k<2; k++) {
+					utils::Coordinate<3> cur({pos[0]+i,pos[1]+j,pos[2]+k});
+					Es[i][j][k] = field[cur].E;
+				}
+			}
+		}
+
+		// compute curl of E on central points
+		curl = 0;
+		double compZDY, compYDZ;
+		double compXDZ, compZDX;
+		double compYDX, compXDY;
+
+		// curl - X
+		compZDY = .25 * (Es[0][1][0].z - Es[0][0][0].z + Es[0][1][1].z - Es[0][0][1].z + Es[1][1][0].z - Es[1][0][0].z + Es[1][1][1].z - Es[1][0][1].z) / universeProperties.cellWidth.y;
+		compYDZ = .25 * (Es[0][0][1].y - Es[0][0][0].y + Es[1][0][1].y - Es[1][0][0].y + Es[0][1][1].y - Es[0][1][0].y + Es[1][1][1].y - Es[1][1][0].y) / universeProperties.cellWidth.z;
+		curl.x = compZDY - compYDZ; 
+
+		// curl - Y
+		compXDZ = .25 * (Es[0][0][1].x - Es[0][0][0].x + Es[1][0][1].x - Es[1][0][0].x + Es[0][1][1].x - Es[0][1][0].x + Es[1][1][1].x - Es[1][1][0].x) / universeProperties.cellWidth.z;
+		compZDX = .25 * (Es[1][0][0].z - Es[0][0][0].z + Es[1][0][1].z - Es[0][0][1].z + Es[1][1][0].z - Es[0][1][0].z + Es[1][1][1].z - Es[0][1][1].z) / universeProperties.cellWidth.x;
+		curl.y = compXDZ - compZDX; 
+
+		// curl - Z
+		compYDX = .25 * (Es[1][0][0].y - Es[0][0][0].y + Es[1][0][1].y - Es[0][0][1].y + Es[1][1][0].y - Es[0][1][0].y + Es[1][1][1].y - Es[0][1][1].y) / universeProperties.cellWidth.x;
+		compXDY = .25 * (Es[0][1][0].x - Es[0][0][0].x + Es[0][1][1].x - Es[0][0][1].x + Es[1][1][0].x - Es[1][0][0].x + Es[1][1][1].x - Es[1][0][1].x) / universeProperties.cellWidth.y;
+		curl.z = compYDX - compXDY; 
+	}
+
+	/**
 	* TODO: provide a description of how the static field solver works
 	*/
 	void solveFieldStatically(const UniverseProperties& universeProperties, const utils::Coordinate<3>& pos, Field& field) {
@@ -121,10 +165,20 @@ namespace ipic3d {
 				
 				// 2. Compute B
 				// 		curlN2C()
+				Vector3<double> curlE;
+				curlN2C(universeProperties, pos, field, curlE);
+
 				//		scale curl by -cdt
+				curlE = curlE * universeProperties.dt;
+
 				//		update B_{n+1} on the center with the computed value
-				//		Boundary conditions: periodic?
+				//		TODO: B should be defined in the center
+				field[pos].Bc = field[pos].Bc - curlE;
+
+				//		TODO: Boundary conditions: periodic?
+						
 				//		interpC2N
+				// 		interpolate B from center to nodes			
 				break;
 			}
 
@@ -185,3 +239,4 @@ namespace ipic3d {
 	}
 
 } // end namespace ipic3d
+
