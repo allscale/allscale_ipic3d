ff1c08714cc40722cb8d33588f65cef65adac088
Roman Iakymchuk <riakymch@kth.se>
2017-09-28 16:46:34 +0200

    Added a reference files for test and tiny benchmarks

---
+++ scripts/run_salloc_24cores.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# The name of the script is myjob
+#SBATCH -J myjob
+
+#SBATCH -A 2017-21
+
+# 100 hours wall-clock time will be given to this job
+#SBATCH -t 08:00:00
+
+# Get an email once it is ready
+#SBATCH --mail-type=ALL 
+
+#SBATCH -C Haswell
+
+# Number of nodes (computers)
+#SBATCH --nodes=1
+
+sleep 10d
+
